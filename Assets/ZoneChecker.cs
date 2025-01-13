95c13ed233fcd36d93419e9839869787f18d3bd9
samuelnihoul <69475809+samuelnihoul@users.noreply.github.com>
2025-01-13 10:42:05 +0100

    imported the zombies, changed the tag in the UI

--- Assets/ZoneChecker.cs
+++ Assets/ZoneChecker.cs
@@ -10,7 +10,7 @@ public class ZoneChecker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        t = GameObject.FindGameObjectWithTag("GameManager").GetComponent<TextMeshProUGUI>();
+        t = GameObject.FindGameObjectWithTag("Respawn").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
